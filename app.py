1723709bcf327b067b88a2bb8fe7e241286dc006
Pablo Corbalán De Concepción <63709477+PabloCorbCon@users.noreply.github.com>
2020-09-07 12:21:32 +0200

    f:home() has been created in app.py

---
+++ app.py
@@ -0,0 +1,20 @@
+# This is the main file of the Pythun Murcia organization, it has been built 
+# using the Flask python module.
+
+# imports
+from flask import Flask, render_template
+
+# create the app object
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+	"""
+	This is the function that manages the main page of
+	the web, it uses the return_template() flask function
+	and the index.html template located in ./templates/
+	to return the main page to the browser.
+	"""
+	return 'main page'
+if __name__ == '__main__':
+	app.run(debug=True)
