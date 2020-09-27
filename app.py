3375b41fa13446cb23d3a6e1a5ffce63c2c0f0f7
PabloCorbCon <pablocorbcon@gmail.com>
2020-09-27 14:55:43 +0200

    500, 404, 403 & 410 errors handled

--- app.py
+++ app.py
@@ -37,12 +37,44 @@ def home():
 	return render_template('index.html', cards=cards)
 
 @app.errorhandler(404)
-def error404(err):
+@app.route('/error/404')
+def error404(err=None):
 	e404 = {
 		'number':'404',
 		'title':'Página no encontrada',
-		'description':'Comprueba que la dirección del enlace esté bien escrita.'}
-	return render_template('error.html', title='404', error=e404)
+		'description':'Comprueba que la dirección del enlace esté bien escrita.'
+	}
+	return render_template('error.html', title='404', error=e404), 404
+
+@app.errorhandler(403)
+@app.route('/error/403')
+def error403(err=None):
+	e403 = {
+		'number':'403',
+		'title':'Acceso denegado',
+		'description':'No tienes permisos para acceder a está página.'
+	}
+	return render_template('error.html', title='403', error=e403), 403
+
+@app.errorhandler(410)
+@app.route('/error/410')
+def error410(err=None):
+	e410 = {
+		'number':'410',
+		'title':'Página no disponible',
+		'description':'Está página ya no está disponible, puede que no este disponible temporalmente.'
+	}
+	return render_template('error.html', title='410', error=e410), 410 
+
+@app.errorhandler(500)
+@app.route('/error/500')
+def error500(err=None):
+	e500 = {
+		'number':'500',
+		'title':'Error interno',
+		'description':'No te preocupes, no es culpa tuya. Estamos intentando arreglar esto.'
+	}
+	return render_template('error.html', title='500', error=e500), 500
 
 if __name__ == '__main__':
 	app.run(debug=True)
